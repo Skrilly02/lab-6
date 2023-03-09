5be2b673e690cc6884bc84ac5c768441d9e9fd1e
unknown <andrewcoffey@hotmail.com>
2023-03-09 15:50:14 +0000

    Lab 6

---
+++ src/app/Services/student.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentService {
+
+  constructor(private http:HttpClient) {}
+
+  getStudentData():Observable<any>{
+    return this.http.get('https://jsonblob.com/api/jsonblob/1080824492259033088');
+    
+  }
+
+  getWeatherData():Observable<any> {
+    return this.http.get('http://api.openweathermap.org/data/2.5/weather?q=Galway&appid=6a66416403ed8e5e6e762cb8c261f303');
+  }
+}
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StudentService } from './Services/student.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'student-app';
+  students:any[]=[];
+  weather:any[]=[];
+  location:any[]=[];
+
+  ngOnInit(): void {
+    this.service.getWeatherData().subscribe(
+      (data) => {
+      this.weather = data.weather;
+      this.location = data.name;
+
+    });
+
+    this.service.getStudentData().subscribe(
+      (data) => {
+    this.students = data.students;
+    });
+  }
+  constructor(private service:StudentService){
+  }
+
 }
